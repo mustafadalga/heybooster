4ffd380d4a1c5ec90ea0777336bfb43501d5acb0
Mustafa Dalga <mustafadalgaa@gmail.com>
2021-02-07 21:39:15 +0300

    Pure js code converted to Vue js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,72 @@
-/* Navigation Menu */
-var container = document.querySelector('.container');
-var hamburger_menu = document.querySelectorAll('.hamburger-menu-wrap');
-hamburger_menu.forEach(element => {
-    element.addEventListener('click', () => {
-        container.classList.toggle('open')
-    });
-});
+const App = {
+    data() {
+        return {
+            titleList: []
+        }
+    },
+    created() {
+        this.getData()
+    },
+    mounted() {
+        this.sideMenu()
+        this.customSelectOption()
+    },
+    methods: {
+        sideMenu() {
+            const container = this.$refs.container;
+            const hamburger_menu = [this.$refs.hamburger_menu1, this.$refs.hamburger_menu2];
+            hamburger_menu.forEach(element => {
+                element.addEventListener('click', () => {
+                    container.classList.toggle('open')
+                });
+            });
+        },
+        async getData() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+                this.titleList = response.data
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        customSelectOption() {
+            /*Custom Select Option */
+            $(".custom-select").each(function() {
+                var classes = $(this).attr("class"),
+                    id = $(this).attr("id"),
+                    name = $(this).attr("name");
+                var template = '<div class="' + classes + '">';
+                template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+                template += '<div class="custom-options">';
+                $(this).find("option").each(function() {
+                    template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+                });
+                template += '</div></div>';
 
-
-
-
-
-/*Custom Select Option */
-$(".custom-select").each(function() {
-    var classes = $(this).attr("class"),
-        id = $(this).attr("id"),
-        name = $(this).attr("name");
-    var template = '<div class="' + classes + '">';
-    template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
-    template += '<div class="custom-options">';
-    $(this).find("option").each(function() {
-        template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
-    });
-    template += '</div></div>';
-
-    $(this).wrap('<div class="custom-select-wrapper"></div>');
-    $(this).hide();
-    $(this).after(template);
-});
-$(".custom-option:first-of-type").hover(function() {
-    $(this).parents(".custom-options").addClass("option-hover");
-}, function() {
-    $(this).parents(".custom-options").removeClass("option-hover");
-});
-$(".custom-select-trigger").on("click", function() {
-    $('html').one('click', function() {
-        $(".custom-select").removeClass("opened");
-    });
-    $(this).parents(".custom-select").toggleClass("opened");
-    event.stopPropagation();
-});
-$(".custom-option").on("click", function() {
-    $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
-    $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
-    $(this).addClass("selection");
-    $(this).parents(".custom-select").removeClass("opened");
-    $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
-});
-
-
-
-// {JSON} Placeholder API
-
-const results = document.querySelectorAll('.filter-result-item');
-const getRequest = async() => {
-    try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        results.forEach((item, index) => {
-            item.querySelector('.filter-result-desc').textContent = response.data[index].title
-        })
-    } catch (err) {
-        console.error(err);
+                $(this).wrap('<div class="custom-select-wrapper"></div>');
+                $(this).hide();
+                $(this).after(template);
+            });
+            $(".custom-option:first-of-type").hover(function() {
+                $(this).parents(".custom-options").addClass("option-hover");
+            }, function() {
+                $(this).parents(".custom-options").removeClass("option-hover");
+            });
+            $(".custom-select-trigger").on("click", function() {
+                $('html').one('click', function() {
+                    $(".custom-select").removeClass("opened");
+                });
+                $(this).parents(".custom-select").toggleClass("opened");
+                event.stopPropagation();
+            });
+            $(".custom-option").on("click", function() {
+                $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+                $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+                $(this).addClass("selection");
+                $(this).parents(".custom-select").removeClass("opened");
+                $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+            });
+        }
     }
-};
-
-getRequest()
+}
+Vue.createApp(App).mount('#app')
